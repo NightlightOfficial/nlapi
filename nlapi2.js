2eb2832f5c799e1b714ac4380b61dd0e5f206a1e
DavidFlashCZ <itenerai@gmail.com>
2022-06-26 17:14:49 +0200

    0.0.1

---
+++ nlapi2.js
@@ -0,0 +1,123 @@
+console.log("%cThank you for using Nightlight API!", "background: #292929; color: #ae1adb; font-size: x-large");
+console.log("%cMake sure to login with your Nightlight credentials when using function getNLAPI()", "background: #292929; color: white; font-size: small");
+
+export class NIGHTLIGHTAPI {
+
+    constructor(login, password) {
+        this.login = login;
+        this.password = password;
+    }
+
+
+    getUser(username){
+        return new NIGHTLIGHTUSER(username, this.login, this.password);
+    }
+
+    getClub(username){
+        return new NIGHTLIGHTCLUB(username, this.login, this.password);
+    }
+}
+
+class NIGHTLIGHTUSER {
+
+    constructor(username, login, password) {
+        this.login = login;
+        this.password = password;
+        this.username = username;
+    }
+
+    async getAll() {
+        return fetch("https://night-light.cz/nlapi/nlapi2.php?login=" + this.login + "&password=" +
+            this.password + "&getUser=" + this.username).then(async (response) => {
+            return response.json().then((data) => {
+                return data;
+            }).catch((err) => {
+                console.log(err);
+            })
+        });
+    }
+
+    async getDisplayName() {
+        return this.getAll().then((data) => {
+            return data['displayname'];
+        });
+    }
+
+    async getBio() {
+        return this.getAll().then((data) => {
+            return data['bio'];
+        });
+    }
+
+    async getLevel() {
+        return this.getAll().then((data) => {
+            return data['level'];
+        });
+    }
+
+    async getXp() {
+        return this.getAll().then((data) => {
+            return data['xp'];
+        });
+    }
+
+    async getTotalPostsUploaded() {
+        return this.getAll().then((data) => {
+            return data['totalpostsuploaded'];
+        });
+    }
+}
+
+class NIGHTLIGHTCLUB {
+
+    constructor(clubname, login, password) {
+        this.login = login;
+        this.password = password;
+        this.clubname = clubname;
+    }
+
+    async getAll() {
+        return fetch("https://night-light.cz/nlapi/nlapi2.php?login=" + this.login + "&password=" +
+            this.password + "&getClub=" + this.clubname).then(async (response) => {
+            return response.json().then((data) => {
+                return data;
+            }).catch((err) => {
+                console.log(err);
+            })
+        });
+    }
+
+    async getOwner() {
+        return this.getAll().then((data) => {
+            return data['owner'];
+        });
+    }
+
+    async getFollowers() {
+        return this.getAll().then((data) => {
+            return data['followers'];
+        });
+    }
+
+    async getMembers() {
+        return this.getAll().then((data) => {
+            return data['members'];
+        });
+    }
+
+    async getBio() {
+        return this.getAll().then((data) => {
+            return data['bio'];
+        });
+    }
+
+    async getTotalPostsUploaded() {
+        return this.getAll().then((data) => {
+            return data['totalpostsuploaded'];
+        });
+    }
+}
+
+function getNLAPI(login, password) {
+    return new NIGHTLIGHTAPI(login, password);
+}
